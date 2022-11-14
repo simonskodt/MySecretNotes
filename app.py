0bccfdf85980b00cba8634c8b0f5d788fb01db9d
simonskodt <simon@skodt.dk>
2022-11-14 10:29:21 +0100

    chore: remove dummy users

--- app.py
+++ app.py
@@ -31,11 +31,6 @@ CREATE TABLE users (
     password TEXT NOT NULL
 );
 
-INSERT INTO users VALUES(null,"admin", "password");
-INSERT INTO users VALUES(null,"bernardo", "omgMPC");
-INSERT INTO notes VALUES(null,2,"1993-09-23 10:10:10","hello my friend",1234567890);
-INSERT INTO notes VALUES(null,2,"1993-09-23 12:10:10","i want lunch pls",1234567891);
-
 """)
 
 
